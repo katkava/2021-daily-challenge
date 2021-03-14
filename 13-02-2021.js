3d3e06ac1a43e4fda9b5012d1b46c5d099b89add
KatKava <katryn.kav97@gmail.com>
2021-03-14 19:18:38 +0000

    My head is at the wrong end, and remove the time challenges

---
+++ 13-02-2021.js
@@ -0,0 +1,13 @@
+//My head is at the wrong end! 
+
+function fixTheMeerkat(arr) {
+ //your code here 
+  return arr.reverse()
+}
+
+//Remove the time
+
+function shortenToDate(longDate) {
+  // your code here
+  return longDate.split(",").splice(0, 1).toString()
+}
