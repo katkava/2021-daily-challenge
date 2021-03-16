5c373f4acb6f512eb5aa6610c4c6821be42c5bd9
KatKava <katryn.kav97@gmail.com>
2021-03-16 15:19:12 +0000

    Do I get a bonus, incorrect division method

---
+++ 16-02-2021.js
@@ -0,0 +1,13 @@
+//Incorrect division method 
+ const solve = (x, y) => x/y
+
+
+//Do I get a bonus? 
+function bonusTime(salary, bonus) {
+if(bonus === true){
+  return `£${salary*10}`
+}else{
+  return `£${salary}`
+}
+}
+
