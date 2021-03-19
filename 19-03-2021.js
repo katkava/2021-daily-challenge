4ae21ce71516eb4c0d562d39b950c14ca8392ded
KatKava <katryn.kav97@gmail.com>
2021-03-19 22:05:36 +0000

    twice as old

--- 19-03-2021.js
+++ 19-03-2021.js
@@ -3,3 +3,10 @@
 function howManyLightsabersDoYouOwn(name) {
   return name === 'Zach' ? 18 : 0;
 }
+
+//Twice as old 
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here 
+ return Math.abs(dadYearsOld - 2 * sonYearsOld)
+}
