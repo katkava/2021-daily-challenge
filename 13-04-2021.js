d272270384c443e4f0c1c563968da2488483d0a1
KatKava <katryn.kav97@gmail.com>
2021-04-13 23:16:04 +0100

    multiples of 3 and 5 sum

---
+++ 13-04-2021.js
@@ -0,0 +1,19 @@
+
+//add multiple of 3 and 5
+function solution(number){
+  var sum = 0;
+  
+  for(var i = 1;i< number; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      sum += i
+    }
+  }
+  return sum;
+}
+
+
+
+
+
+
+
