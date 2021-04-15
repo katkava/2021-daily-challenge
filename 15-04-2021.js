d86194579566d6e8d4aecb0d4ae28de2a836f766
KatKava <katryn.kav97@gmail.com>
2021-04-15 21:42:48 +0100

    count vowels in string

---
+++ 15-04-2021.js
@@ -0,0 +1,24 @@
+
+//count all the vowels in a string 
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  
+  return vowelsCount;
+}
+
+
+
+
+
+
+
+
