1a18a9f4da4d86fd044b1cb70ecfea15008553f3
KatKava <katryn.kav97@gmail.com>
2021-04-18 12:30:19 +0100

    keep up the hoop

--- 18-04-2021.js
+++ 18-04-2021.js
@@ -11,3 +11,9 @@ function find_average(array) {
   var sum = array.reduce((a, b) => a + b, 0);
   return sum/array.length;
 }
+
+
+//Keep up the hoop 
+function hoopCount (n) {
+  return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+}
