2ba93beaf6d237c708aea75f1a1998889d76805c
KatKava <katryn.kav97@gmail.com>
2021-04-21 21:14:59 +0100

    alan partirdge

---
+++ 21-04-2021-strings.js
@@ -0,0 +1,4 @@
+// Alan Partrigde II - Apple Turnover
+function apple(x){
+  return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+}
