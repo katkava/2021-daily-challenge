285d47ae4e23ba4166a95537898e63469715dca6
KatKava <katryn.kav97@gmail.com>
2021-04-21 21:20:28 +0100

    Template strings

--- 21-04-2021-strings.js
+++ 21-04-2021-strings.js
@@ -1,4 +1,9 @@
-// Alan Partrigde II - Apple Turnover
+// 1) Alan Partrigde II - Apple Turnover
 function apple(x){
   return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+}
+
+// 2) Template strings 
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`;
 }
