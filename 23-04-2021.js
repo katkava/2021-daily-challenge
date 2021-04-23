9ff3e7643a4bdf90027215b56aa07d365c7d5b3e
KatKava <katryn.kav97@gmail.com>
2021-04-23 21:56:00 +0100

    do you play banjo?

---
+++ 23-04-2021.js
@@ -0,0 +1,9 @@
+// Are you playing banjo? 
+
+function areYouPlayingBanjo(name) {
+  if (name[0] === "R" || name[0] === "r"){
+    return `${name} plays banjo`
+  }else{
+    return `${name} does not play banjo`
+  }
+}
