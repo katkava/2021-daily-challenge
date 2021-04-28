fa8d72e0472de6f576a07b5fcc81c38c634764cc
KatKava <katryn.kav97@gmail.com>
2021-04-28 22:10:01 +0100

    area or perimeter

---
+++ 28-04-2021.js
@@ -0,0 +1,10 @@
+//Return area or perimeter based on parameters
+
+const areaOrPerimeter = function(l , w) {
+ //if square return area, if rectangle return perimeter
+  if (l===w){
+    return l*w
+  }else{
+    return l*2 + w*2
+  }
+};
