22b71582cc25ecb2c38440a19a9e1ea2a68b963c
KatKava <katryn.kav97@gmail.com>
2021-05-04 20:27:01 +0100

    filter out geese

--- 04-05-2021.js
+++ 04-05-2021.js
@@ -1,6 +1,15 @@
-// Sum mixed array 
+//1) Sum mixed array 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
 function sumMix(x){
  return x.map(item => parseInt(item)).reduce((a,b) => a+b, 0)
 }
+
+//2) Filter out the geese 
+// return an array containing all of the strings in the input array except those that match strings in geese
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return gooseFilter.pop(geese); 
+};
+
+//3) 
