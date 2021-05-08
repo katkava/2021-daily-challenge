5cec7cf0c6e26199d7d1e2517ef1883f11ea0c85
KatKava <katryn.kav97@gmail.com>
2021-05-08 23:44:26 +0100

    small enough? beginner

--- 08-05-2021.js
+++ 08-05-2021.js
@@ -1,4 +1,4 @@
-//Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+// 1) Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
 
 //In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
 
@@ -11,4 +11,11 @@ function shorter_reverse_longer(a,b){
    }else{
      return a+b.split("").reverse().join("")+a
    }
+}
+
+// 2) Small enough? 
+//You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+function smallEnough(a, limit){
+ return  a.every(el => el<limit || el===limit)
 }
