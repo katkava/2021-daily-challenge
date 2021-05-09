10ae33e735055c588e3ee1d7a278f8d4e03eb115
KatKava <katryn.kav97@gmail.com>
2021-05-09 16:26:03 +0100

    3 array hw class 44

--- class44-hw-arrays.js
+++ class44-hw-arrays.js
@@ -9,12 +9,21 @@ return array.reduce((a, b) => a*b, 1)
 //Two:
 // You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
 
-
-
 function familyIssues(family){
  let finalArray = []
  const minAge = [Math.min(...family)]
  const maxAge = [Math.max(...family)]
  const difMaxMin = maxAge-minAge
  return finalArray.concat(minAge, maxAge, difMaxMin)
+}
+
+// Three:
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+// Example:
+// [ 6, 2, 1, 8, 10 ] => 16
+// [ 1, 1, 11, 2, 3 ] => 6
+
+function addRemoveFirstAndLast(array) {
+let sortedArray = array.sort((a, b) => a-b).slice(1, -1)
+return sortedArray.reduce((a, b) => a+b, 0)
 }
