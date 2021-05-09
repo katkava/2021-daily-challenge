4f678f12fcc1fba4e3e5d1e7aa98195646cd6436
KatKava <katryn.kav97@gmail.com>
2021-05-09 16:10:30 +0100

    array hw questions

--- class44-hw-arrays.js
+++ class44-hw-arrays.js
@@ -9,8 +9,7 @@ return array.reduce((a, b) => a*b, 1)
 //Two:
 // You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
 
-let family = [1, 10, 4, 31, 50]
-let ageDifference = [] 
+
 
 function familyIssues(family){
  let finalArray = []
