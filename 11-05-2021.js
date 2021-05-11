7dea0c617f071311bf2d8a49127cbe8557e81faa
KatKava <katryn.kav97@gmail.com>
2021-05-11 21:44:08 +0100

    find first non consecutive number in array

---
+++ 11-05-2021.js
@@ -0,0 +1,10 @@
+//Find the first non consecutive number in an array 
+
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1]
+    }
+  }
+  return null
+} 
