764f4e6643081f5d2f477ea51d6a66ba8eedb30c
KatKava <katryn.kav97@gmail.com>
2021-05-17 18:06:26 +0100

    array challenge

---
+++ 17-05-2021.js
@@ -0,0 +1,20 @@
+//Coding meetup 
+
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+
+function isSameLanguage(list) {
+  // thank you for checking out the Coding Meetup kata :)
+  return list.every(lang => lang.language === list[0].language)
+}
