d130cc85317703483e3d0a0a1a6e695e27c5dc70
KatKava <katryn.kav97@gmail.com>
2021-05-22 18:53:05 +0100

    5 strings challenges

--- 22-05-2021.js
+++ 22-05-2021.js
@@ -3,3 +3,11 @@
 function reverse(string){
   return string.split(' ').reverse().join(' ');
 }
+
+// Shark pantoon 
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  if(dolphin){
+    sharkSpeed /= 2;
+  }
+  return pontoonDistance/youSpeed < sharkDistance/sharkSpeed ? "Alive!" : "Shark Bait!";
+}
