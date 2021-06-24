f4bed3525c12d243b0d3812ffa42da892477f49b
KatKava <katryn.kav97@gmail.com>
2021-06-24 19:35:10 +0100

    distance between two points geometry

--- 24-06-2021.js
+++ 24-06-2021.js
@@ -12,10 +12,28 @@ function checkAlive(health) {
   return health > 0;
 }
 
-//find subsequent traffic light 
+//3 find subsequent traffic light 
 
 function updateLight(current) {
   
   return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
 
+}
+
+//4 Return array of numbers and exponents until that num 
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+
+function powersOfTwo(n){
+  let array = [] 
+  for(let i = 0; i <= n; i++){
+   array.push(Math.pow(2, i))
+}
+  return array
+  }
+
+  //find distance between 2 points (geometry)
+
+  function distanceBetweenPoints(a, b) {
+  
+  return Math.hypot(a.x-b.x, a.y-b.y)
 }
