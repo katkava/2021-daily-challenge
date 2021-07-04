cb71b9c162478922fa4452bb2f25ea8674fd6f23
KatKava <katryn.kav97@gmail.com>
2021-07-04 12:50:46 +0100

    count by X

---
+++ 04-07-2021.js
@@ -0,0 +1,17 @@
+
+//Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+//Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+//Return the results as an array (or list in Python, Haskell or Elixir).
+
+
+
+
+const countBy = (x, n) => {
+    let z = [];
+   for(let i = 1; i <= n; i++){
+     z.push(i*x)
+   }
+    return z
+  }
