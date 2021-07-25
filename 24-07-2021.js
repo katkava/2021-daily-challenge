1bd9dba51cd8b8a8046874de6e4e55fd8287a725
KatKava <katryn.kav97@gmail.com>
2021-07-25 12:39:39 +0100

    added extra solutino using charCodeAt for sorting by last char problem

--- 24-07-2021.js
+++ 24-07-2021.js
@@ -1,6 +1,14 @@
 //Sort by last char. Given a string of words, organise them so that they are sorted alphabetically by their last letter. If words with same letters appear, sort them by the order they first appear in the original string. 
 
-function last(x){
+
+//solution one using greater than to manipulate the logic behind sort. 
+function last(x){0
     return x.split(" ").sort((a,b) => a[a.length -1] > b[b.length -1])   
    
  }
+
+ //solution two with charCodeAt  
+
+ function last(x){
+    return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+  }
