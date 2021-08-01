d8b74166b017e27d5c89587b3e5b24679294d38c
KatKava <katryn.kav97@gmail.com>
2021-08-01 23:00:34 +0100

    sum of digital roots

--- 1-08-2021.js
+++ 1-08-2021.js
@@ -14,3 +14,13 @@ function findOdd(A) {
         
     }
   }
+
+  // SUm of digital root 
+
+  function digital_root(n){
+    let result = 0;
+    n.toString().split('').map (n => {
+    result += Number(n)
+    })
+    return result > 9 ? digital_root(result) : result;
+    }
